0467ad07a82d445a45d4eebe825ac7d54f5a1c22
Mathias Huaman <mathiauni2007@gmail.com>
2025-11-07 00:35:09 -0500

    Main con mis datos personales

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PRACTICA_SEMANA_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Nombres y Apellidos: Mathias David Huaman Espiritu   ");
+
+
+
+        }
+    }
+}
